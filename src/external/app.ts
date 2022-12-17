aecb58d5787fb74ad93b5fea8710d633a017ba92
Alain Marquez <alamar1990@gmail.com>
2022-12-17 18:14:43 -0500

    Init

---
+++ src/external/app.ts
@@ -0,0 +1,28 @@
+import cors from 'cors'
+import express from 'express'
+
+// import { apiRoutes } from './routes'
+
+const app = express()
+
+app.disable('x-powered-by')
+
+const options: cors.CorsOptions = {
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  preflightContinue: true
+}
+
+app.use(express.json())
+app.use(cors(options))
+
+// app.use(apiRoutes)
+
+// Test route to /
+app.get('/', (_req, res) => {
+  res.send({ message: 'Taxator API started' })
+})
+
+export { app }
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { app } from './external/app'
+// import { connection } from './database/dbConfig'
+
+const port = process.env.PORT || 5000
+
+// connection.then(() => { console.log('Database connected') }).catch((err) => console.log('Error connecting to database:', err))
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
