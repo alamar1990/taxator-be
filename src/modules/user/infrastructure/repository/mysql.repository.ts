14c7c380c7be29abf6ee2dae6f85d7e31bb26fd6
Alain Marquez <alamar1990@gmail.com>
2022-12-20 11:46:01 -0500

    Adjust types

--- src/modules/user/infrastructure/repository/mysql.repository.ts
+++ src/modules/user/infrastructure/repository/mysql.repository.ts
@@ -9,18 +9,18 @@ import UserModel from '../model/user.schema.sequelize'
  * Mysql!
  */
 export class MySqlRepository implements UserRepository {
-  async findUserById(id: number): Promise<any> {
+  async findUserById(id: number): Promise<UserEntity> {
     const user = await UserModel.findByPk({ id })
     return user
   }
 
-  async registerUser(userIn: UserEntity): Promise<any> {
+  async registerUser(userIn: UserEntity): Promise<UserEntity> {
     const user = new UserModel(userIn)
     await user.save()
     return user
   }
 
-  async listUsers(): Promise<any> {
+  async listUsers(): Promise<UserEntity[]> {
     const users = await UserModel.findAll()
     return users
   }
