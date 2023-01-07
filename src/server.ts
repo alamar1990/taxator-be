d634453489e4962b0fcfe460f7ae1e63793245bc
Alain Marquez <alamar1990@gmail.com>
2023-01-07 01:00:36 -0500

    Add process env list on start

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,6 @@ import { config } from './config'
 const PORT = config.PORT || 5000
 
 app.listen(PORT, () => {
+  console.log(process.env)
   console.log(`🚀 Application Taxator-Backend running on PORT -> ${PORT}`)
 })
