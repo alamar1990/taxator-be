f008f8b8115396a5f0b520d1d6b5f608beb5fb4f
Alain Marquez <alamar1990@gmail.com>
2023-02-07 17:15:51 -0500

    Update docker compose file

---
+++ docker/db/01-init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO postgres;
+  \connect $APP_DB_NAME postgres
+  COMMIT;
+EOSQL
---
+++ docker/db/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE taxator;
+GRANT ALL PRIVILEGES ON DATABASE taxator TO postgres;
+connect taxator postgres
+COMMIT;
